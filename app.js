221ed46668fedc9925356dcc639ef4afbc11a1f5
Unreala9 <shwetchourey3@gmail.com>
2024-06-05 10:52:05 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,124 +1,130 @@
-//including the dependencies
-var express 	= require('express');
-var app 		= express();
-var bodyParser  = require("body-parser");
-var mongoose 	= require("mongoose");
+// Including the dependencies
+require("dotenv").config(); // Add this line to use environment variables
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-
-
-// setting view engine
+// Setting view engine
 app.set("view engine", "ejs");
-app.use(express.static(__dirname+'/public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// schema setup
-mongoose.connect("mongodb://localhost:27017/attendance");
-var facultyRegSchema = new mongoose.Schema({
-	name: String,
-	pass: String
+// MongoDB connection setup
+const dbURI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected...");
+  })
+  .catch((err) => {
+    console.error("Connection error", err.message);
+  });
+
+// Schema setup
+const facultyRegSchema = new mongoose.Schema({
+  name: String,
+  pass: String,
 });
 
-var Faculty = mongoose.model("Faculty", facultyRegSchema);	
+const Faculty = mongoose.model("Faculty", facultyRegSchema);
 
-//	Student Schema
-var studentSchema = new mongoose.Schema({
-	name: String,
-	rollNo: Number,
-	attendance: Number
+// Student Schema
+const studentSchema = new mongoose.Schema({
+  name: String,
+  rollNo: Number,
+  attendance: Number,
 });
 
-var Student = mongoose.model("Student", studentSchema);
+const Student = mongoose.model("Student", studentSchema);
 
-Student.create({
-	name: "Abhishek",
-	rollNo: 601,
-	attendance: 1
-}, function(err, Student){
-	if(err){
-		console.log(err);
-	} 
-	else{
-		console.log("Newly Created Student");
-		console.log(Student);
-	}
+Student.create(
+  {
+    name: "Abhishek",
+    rollNo: 601,
+    attendance: 1,
+  },
+  function (err, student) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Newly Created Student");
+      console.log(student);
+    }
+  }
+);
+
+// Routing
+// INDEX PAGE
+app.get("/landing", function (req, res, next) {
+  res.render("landing");
 });
 
-// routing 
-//	INDEX PAGE
-app.get('/landing',function(req,res,next){
-	res.render("landing");
+// Login page/register
+app.get("/login", function (req, res, next) {
+  res.render("login.ejs");
 });
 
-//	login page/register
-app.get('/login',function(req, res, next){
-	res.render("login.ejs");
+// Storing user data from registration page
+app.post("/home", function (req, res, next) {
+  const regname = req.body.regname;
+  const regpass = req.body.regpass;
+  const credentials = { name: regname, pass: regpass };
+  Faculty.create(credentials, function (err, newlyCreated) {
+    if (err) {
+      console.log(err);
+    } else {
+      // Direct to home page which is to be constructed
+      res.redirect("/home");
+    }
+  });
 });
 
-//	storing user data from registration page
-app.post("/home", function(req, res, next){
-	var regname = req.body.regname;
-	var regpass = req.body.regpass;
-	var Credentials = {name: regname, pass: regpass};
-	Faculty.create(Credentials, function(err, newlyCreated){
-		if(err){
-			console.log(err);
-		} else {
-			//direct to home page which is to be constructed
-			res.redirect("/home");
-		}
-	});
-	//res.send("U hit post route");
+app.get("/home", function (req, res, next) {
+  res.render("home");
 });
 
-app.get("/home", function(req, res, next){
-	res.render("home");
+app.get("/login/home", function (req, res, next) {
+  res.render("login/home");
 });
 
-app.get("/login/home", function(req, res, next){
-	res.render("login/home");
+// Attendance marking
+app.get("/attendance", function (req, res, next) {
+  res.render("attendance");
 });
 
-//	attendance marking
-app.get("/attendance", function(req, res, next){
-	res.render("attendance");
+// Post method to add data to db
+app.post("/faculty", function (req, res, next) {
+  const name = req.body.name;
+  const department = req.body.department;
+  const newFaculty = { name: name, department: department };
+  Faculty.create(newFaculty, function (err, newlyCreated) {
+    if (err) {
+      console.log(err);
+    } else {
+      // Redirect to faculty page
+      res.redirect("/faculty");
+    }
+  });
 });
 
-
-
-//post method to add data to db
-app.post("/faculty",function(req, res, next){
-	//res.send("You hit the post route");
-	//get the data from faculty array and add to it
-	var name = req.body.name;
-	var department = req.body.department;
-	var newFaculty = {name: name, department: department};
-	//faculty.push(newFaculty);
-	Faculty.create(newFaculty, function(err, newlyCreated){
-		if(err){
-			console.log(err);
-		}
-		else{
-			//redirect to faculty page
-			res.redirect("/faculty");
-		}
-	});
+// Form for adding faculty
+app.get("/faculty/newFaculty", function (req, res, next) {
+  res.render("newFaculty.ejs");
 });
 
-
-
-//form for adding faculty
-app.get("/faculty/newFaculty", function(req, res, next){
-	res.render("newFaculty.ejs");
+// Adding route to view specific faculty
+app.get("/faculty/:id", function (req, res, next) {
+  res.send("Show specific faculty");
 });
 
-// adding route to view specific faculty
-app.get("/faculty/:id", function(req, res, next){
-	res.send("Show specific faculty");
-});
-
-app.listen(process.env.PORT || 3000, process.env.IP, function(){
-	console.log("Server started");
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server started");
 });
 
 // Faculty.create(
@@ -140,7 +146,6 @@ app.listen(process.env.PORT || 3000, process.env.IP, function(){
 // 		{name: "Govind" , department: "IT"},
 // 		{name: "Dalgade", department: "CS"}
 // 	];
-
 
 //landing on faculty form
 // app.get("/faculty", function(req, res, next){
